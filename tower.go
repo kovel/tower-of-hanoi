8a142f3d7c5ee3cf84af717ae384decf68db0de2
Alexander Kovel <kovelav@gmail.com>
2017-06-30 12:22:47 +0300

    sample "towers of hanoi" computation

---
+++ tower.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+const DISKS = 3;
+
+func doTowers(topN int, from rune, inter rune, to rune, padding string) {
+	if topN == 1 {
+		fmt.Printf("Disk 1 from %c to %c\n", from,  to)
+	} else {
+		doTowers(topN - 1, from, to, inter, padding + "\t")
+		fmt.Printf("%sDisk %d from %c to %c\n", padding, topN, from, to)
+		doTowers(topN - 1, inter, from, to, padding + "\t")
+	}
+}
+
+func main() {
+	doTowers(DISKS, 'A', 'B', 'C', "")
+}
