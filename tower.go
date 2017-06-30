6fcfb6e3eb28017dbfa5862dfe03e6f698bc5db4
Alexander Kovel <kovelav@gmail.com>
2017-06-30 12:23:46 +0300

    fixed formatting

--- tower.go
+++ tower.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 )
 
-const DISKS = 3;
+const DISKS = 3
 
 func doTowers(topN int, from rune, inter rune, to rune, padding string) {
 	if topN == 1 {
-		fmt.Printf("Disk 1 from %c to %c\n", from,  to)
+		fmt.Printf("Disk 1 from %c to %c\n", from, to)
 	} else {
-		doTowers(topN - 1, from, to, inter, padding + "\t")
+		doTowers(topN-1, from, to, inter, padding+"\t")
 		fmt.Printf("%sDisk %d from %c to %c\n", padding, topN, from, to)
-		doTowers(topN - 1, inter, from, to, padding + "\t")
+		doTowers(topN-1, inter, from, to, padding+"\t")
 	}
 }
 
